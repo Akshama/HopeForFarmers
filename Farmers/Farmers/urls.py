75bdfe8a2ea17ca7d0e9061b8c58f3c238dc3791
Akshama <akshama96@gmail.com>
2017-10-28 06:35:54 +0530

    Login view updated-working with one user for now :3

--- Farmers/Farmers/urls.py
+++ Farmers/Farmers/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	url(r'', include('hope.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login),
 
 ]
 
